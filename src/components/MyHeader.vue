0024e69062c3d2360fc90649159d8b98a83763f2
Orlando Gonzalez Albarran <ogonzaleza1@toluca.tecnm.mx>
2022-10-06 12:40:40 -0500

    navbar and carousel modifications

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,9 +1,14 @@
 <template>
   <nav class="navbar navbar-expand-lg header-text">
-    <div class="container-fluid container">
+    <div class="container px-4">
       <!--Logo-->
       <router-link class="nav-link active" aria-current="page" to="/">
-        <img src="@/assets/img/vision-y-estrategia.png" width="120" height="" />
+        <img
+          src="@/assets/img/vision-y-estrategia.png"
+          width="180"
+          height="110"
+          alt="logo"
+        />
       </router-link>
       <!--Navbar toggler-->
       <button
@@ -18,21 +23,24 @@
         <span class="navbar-toggler-icon"></span>
       </button>
       <!--Navbar-->
-      <div class="collapse navbar-collapse" id="navbarNav">
+      <div
+        class="collapse navbar-collapse justify-content-center"
+        id="navbarNav"
+      >
         <ul class="navbar-nav">
-          <li class="nav-item">
+          <li class="nav-item px-3">
             <router-link class="nav-link active" aria-current="page" to="/">
               <i class="fa fa-home"></i>
               <b>Inicio</b>
             </router-link>
           </li>
-          <li class="nav-item">
+          <li class="nav-item px-3">
             <router-link class="nav-link" to="/nosotros">
               <i class="fa fa-phone"></i>
               <b>Nosotros</b>
             </router-link>
           </li>
-          <li class="nav-item dropdown">
+          <li class="nav-item px-3 dropdown">
             <a
               class="nav-link dropdown-toggle"
               href="#"
@@ -61,16 +69,10 @@
               </li>
             </ul>
           </li>
-          <li class="nav-item">
+          <li class="nav-item px-3">
             <router-link class="nav-link" to="/contacto">
               <i class="fa fa-phone"></i>
               <b>Contacto</b>
-            </router-link>
-          </li>
-          <li class="nav-item">
-            <router-link class="nav-link" to="/template">
-              <i class="fa fa-folder"></i>
-              <b>Template</b>
             </router-link>
           </li>
         </ul>
