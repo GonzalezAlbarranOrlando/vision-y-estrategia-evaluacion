42159c663c0b635b7bf482aef25c2f9a327e9a7e
Orlando Gonzalez Albarran <ogonzaleza1@toluca.tecnm.mx>
2022-10-09 20:50:20 -0500

    footer design and functionality

--- src/components/MyFooter.vue
+++ src/components/MyFooter.vue
@@ -1,19 +1,104 @@
 <template>
-    <div class="footer-text">
-        <div class="container">
-            <footer>
-                FOOTER
-            </footer>
+  <div class="footer-text">
+    <div class="container py-3">
+      <footer>
+        <div class="row">
+          <div class="col-md-3 center" style="text-align: center">
+            <i class="fa fa-envelope-o"></i>
+            <br />
+            <a href="mailto: name@example.com" class="anchor-footer-text"
+              >name@example.com</a
+            >
+          </div>
+          <div class="col-md-3 center" style="text-align: center">
+            <i class="fa fa-phone"></i>
+            <br />
+            <i class="fa fa-building-o"></i>
+            &nbsp;
+            <a href="tel:123-456-7890" class="anchor-footer-text"
+              >123-456-7890</a
+            >
+            <br />
+            <i class="fa fa-volume-control-phone"></i>
+            &nbsp;
+            <a href="tel:123-456-7890" class="anchor-footer-text"
+              >123-456-7890</a
+            >
+            <br />
+            <i class="fa fa-whatsapp"></i>
+            &nbsp;
+            <a
+              href="https://api.whatsapp.com/send?phone=1234567890"
+              class="anchor-footer-text"
+              target="_blank"
+              >123-456-7890</a
+            >
+          </div>
+          <div class="col-md-3 center" style="text-align: center">
+            <i class="fa fa-map-marker"></i>
+            <br />
+            <a
+              href="https://goo.gl/maps/MWneWd57QfmDY1tY7"
+              class="anchor-footer-text"
+              target="_blank"
+              >Ex Hacienda Barbabosa 100-A Col. San Miguel Zinacantepec C.P.
+              51350</a
+            >
+          </div>
+          <div class="col-md-3 center" style="text-align: center">
+            <i class="fa fa-clock-o"></i>
+            <br />
+            9:00 a.m. - 6:00 p.m.
+          </div>
+          <div class="w-100"></div>
+          <div class="col-12 center">
+            Mantente en Contacto &nbsp;
+            <a
+              href="https://www.linkedin.com/company/maxancertificaci%C3%B3n/"
+              class="anchor-footer-text"
+              target="_blank"
+            >
+              <i class="fa fa-linkedin-square"></i>
+            </a>
+            &nbsp;
+            <a
+              href="https://www.facebook.com/maxancertificacion"
+              class="anchor-footer-text"
+              target="_blank"
+            >
+              <i class="fa fa-facebook-square"></i>
+            </a>
+            &nbsp;
+            <a
+              href="https://twitter.com/maxan_certifica"
+              class="anchor-footer-text"
+              target="_blank"
+            >
+              <i class="fa fa-twitter-square"></i>
+            </a>
+            &nbsp;
+            <a
+              href="https://www.youtube.com/channel/UCsPeus8MPPsGmjX6O1Yuvcw"
+              class="anchor-footer-text"
+              target="_blank"
+            >
+              <i class="fa fa-youtube-square"></i>
+            </a>
+          </div>
         </div>
+      </footer>
     </div>
+  </div>
+  <div class="container-fluid derechos-reservados">
+    Todos los derechos reservados.
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'MyFooter'
-}
+  name: "MyFooter",
+};
 </script>
 
 <style>
-
 </style>
