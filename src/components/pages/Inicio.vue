aa9e698441bb33d8b0a373e469ea3ae2d3212c6f
Orlando Gonzalez Albarran <ogonzaleza1@toluca.tecnm.mx>
2022-10-19 21:52:43 -0500

    aviso de privacidad

--- src/components/pages/Inicio.vue
+++ src/components/pages/Inicio.vue
@@ -99,6 +99,15 @@
         <!--card servicios agpr end-->
       </div>
     </div>
+    <div class="alert alert-danger my-5" role="alert">
+      Favor de leer el
+      <a
+        href="/pdf/AVISO DE PRIVACIDAD Y CONFIDENCIALIDAD.pdf"
+        download
+        class="alert-link"
+        >Aviso de privacidad</a
+      >.
+    </div>
   </div>
 </template>
 
