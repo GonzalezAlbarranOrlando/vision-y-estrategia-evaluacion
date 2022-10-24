931e995eaaa5d7340a380eed042436d6e3ed938a
Orlando Gonzalez Albarran <ogonzaleza1@toluca.tecnm.mx>
2022-10-23 19:06:10 -0500

    inicio images

--- src/components/pages/Inicio.vue
+++ src/components/pages/Inicio.vue
@@ -20,6 +20,12 @@
           data-bs-slide-to="1"
           aria-label="Slide 2"
         ></button>
+        <button
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide-to="2"
+          aria-label="Slide 3"
+        ></button>
       </div>
       <div class="carousel-inner">
         <div class="carousel-item active" data-bs-interval="3000">
@@ -35,6 +41,15 @@
           <router-link to="/pbr">
             <img
               src="@/assets/img/carrusel/carrusel-PbR.jpeg"
+              class="d-block w-100"
+              alt="..."
+            />
+          </router-link>
+        </div>
+        <div class="carousel-item" data-bs-interval="3000">
+          <router-link to="/agpr">
+            <img
+              src="@/assets/img/carrusel/carrusel-agpr.jpeg"
               class="d-block w-100"
               alt="..."
             />
@@ -66,7 +81,11 @@
         <!--card servicios pae-->
         <div class="card">
           <router-link to="/pae" style="text-decoration: none">
-            <img src="@/assets/img/example.jpg" class="w-100" alt="..." />
+            <img
+              src="@/assets/img/servicios/servicio-pae.jpeg"
+              class="w-100"
+              alt="..."
+            />
             <div class="card-header table-title">
               <h5 class="my-3">Programa Anual de Evaluación (PAE)</h5>
             </div>
@@ -78,7 +97,11 @@
         <!--card servicios pbr-->
         <div class="card">
           <router-link to="/pbr" style="text-decoration: none">
-            <img src="@/assets/img/example.jpg" class="w-100" alt="..." />
+            <img
+              src="@/assets/img/servicios/servicio-pbr.jpeg"
+              class="w-100"
+              alt="..."
+            />
             <div class="card-header table-title">
               <h5 class="my-3">Presupuesto basado en Resultados (PbR)</h5>
             </div>
@@ -90,7 +113,11 @@
         <!--card servicios agpr-->
         <div class="card">
           <router-link to="/agpr" style="text-decoration: none">
-            <img src="@/assets/img/example.jpg" class="w-100" alt="..." />
+            <img
+              src="@/assets/img/servicios/servicio-agpr.jpeg"
+              class="w-100"
+              alt="..."
+            />
             <div class="card-header table-title">
               <h5 class="my-3">Estándar de Competencia</h5>
             </div>
